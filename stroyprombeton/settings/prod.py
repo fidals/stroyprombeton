76ea37fc3260a383b6707f9bea4d19876e941712
duker <duker33@gmail.com>
2017-01-25 14:58:22 +0300

    Fix prod.py settings

--- stroyprombeton/settings/prod.py
+++ stroyprombeton/settings/prod.py
@@ -3,9 +3,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # http->https change
-os.environ['HTTPS'] = 'on'
-
-YANDEX_KASSA_LINK = 'https://money.yandex.ru/eshop.xml'
+os.environ['HTTPS'] = 'off'
