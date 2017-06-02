75e1ca5052146d7847e141e9c56e4ef888665390
Oleg <shvedovskiy@users.noreply.github.com>
2017-06-02 12:40:23 +0300

    Filters by diplicate products in admin (#96)

--- stroyprombeton/admin.py
+++ stroyprombeton/admin.py
@@ -1,6 +1,10 @@
+from itertools import chain
+
 from django import forms
 from django.contrib import admin
+from django.contrib.postgres.aggregates import ArrayAgg
 from django.contrib.redirects.models import Redirect
+from django.db.models import Func, Value
 from django.utils.translation import ugettext_lazy as _
 
 from pages import models as pages_models
@@ -26,6 +30,47 @@ class ParentFilter(admin.SimpleListFilter):
             return
 
         return queryset.filter(parent__slug=self.value())
+
+
+class CharacteristicsEqualityFilter(admin.SimpleListFilter):
+    title = _('characteristics equality')
+    parameter_name = 'equality'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('h1', _('Only same h1')),
+            ('all', _('Same h1, characteristics, price'))
+        )
+
+    def queryset(self, request, queryset):
+        if not self.value():
+            return
+
+        def find_duplicates_by_field(queryset, field_name):
+            return set(chain.from_iterable(
+                queryset
+                .values(field_name)
+                .annotate(ids=ArrayAgg('id'))
+                .annotate(c=Func('ids', Value(1), function='array_length'))
+                .order_by()
+                .filter(c__gt=1)
+                .values_list('ids', flat=True)
+            ))
+
+        if self.value() == 'h1':
+            return queryset.filter(id__in=find_duplicates_by_field(queryset, 'h1'))
+
+        if self.value() == 'all':
+            ids = [find_duplicates_by_field(queryset, field_name) for field_name in [
+                'stroyprombeton_product__price', 'stroyprombeton_product__code',
+                'stroyprombeton_product__mark', 'stroyprombeton_product__specification',
+                'stroyprombeton_product__length', 'stroyprombeton_product__width',
+                'stroyprombeton_product__height', 'stroyprombeton_product__weight',
+                'stroyprombeton_product__volume', 'stroyprombeton_product__diameter_out',
+                'stroyprombeton_product__diameter_in', 'description', 'title', 'h1'
+            ]]
+
+            return queryset.filter(id__in=set.intersection(*ids))
 
 
 class SpecificationFilter(admin.SimpleListFilter):
@@ -128,6 +173,13 @@ class StbProductPageAdmin(admin_models.ProductPageAdmin):
     form = CustomWidgetsForm
     category_page_model = stb_models.CategoryPage
     inlines = [StbProductInline, StbImageInline]
+    list_filter = [
+        'is_active',
+        filters.PriceRange,
+        filters.HasContent,
+        filters.HasImages,
+        CharacteristicsEqualityFilter,
+    ]
 
 
 class StbCategoryPageAdmin(admin_models.CategoryPageAdmin):
