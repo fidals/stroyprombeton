80966bec24eee36127956ed9f94d54361224d708
Dima Veselov <d.a.veselov@yandex.ru>
2017-10-13 19:44:16 +0300

    [#119] PDD tasks validation (#120)

--- stroyprombeton/settings/dev.py
+++ stroyprombeton/settings/dev.py
@@ -16,14 +16,14 @@ USE_CELERY = False
 
 """
     @todo #106 Включить в dev-среде django-debug-toolbar по-умолчанию
-      Но в CI его нужно отключать, т.к. возникают ошибки
+     Но в CI его нужно отключать, т.к. возникают ошибки
 
-      def show_toolbar(request):
-          # Always display debug toolbar when running on development config
-          return True
+     def show_toolbar(request):
+         # Always display debug toolbar when running on development config
+         return True
 
-      DEBUG_TOOLBAR_CONFIG = {
-          'SHOW_TOOLBAR_CALLBACK': show_toolbar,
-      }
+     DEBUG_TOOLBAR_CONFIG = {
+         'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+     }
 
 """
