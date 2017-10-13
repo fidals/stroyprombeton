7bff819a8601a81833ba28d13f13261f85f3e726
Dima Veselov <d.a.veselov@yandex.ru>
2017-10-13 12:55:46 +0300

    [#106] Microdata fixes (#108)

--- stroyprombeton/settings/dev.py
+++ stroyprombeton/settings/dev.py
@@ -12,3 +12,18 @@ THUMBNAIL_DEBUG = True
 SITE_DOMAIN_NAME = 'stage.stroyprombeton.ru'
 
 USE_CELERY = False
+
+
+"""
+    @todo #106 Включить в dev-среде django-debug-toolbar по-умолчанию
+      Но в CI его нужно отключать, т.к. возникают ошибки
+
+      def show_toolbar(request):
+          # Always display debug toolbar when running on development config
+          return True
+
+      DEBUG_TOOLBAR_CONFIG = {
+          'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+      }
+
+"""
