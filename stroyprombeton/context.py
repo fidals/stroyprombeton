abbe39027d6312948b7c7c10e13050eac4c58871
duker33 <duker33@gmail.com>
2018-11-02 17:57:18 +0700

    rf#198  Customize product images context (#329)

--- stroyprombeton/context.py
+++ stroyprombeton/context.py
@@ -12,6 +12,8 @@ import typing
 from django.conf import settings
 
 from catalog import context
+from catalog.models import ProductQuerySet
+from images.models import Image
 from pages.models import ModelPage
 
 from stroyprombeton import models as stb_models
@@ -53,3 +55,30 @@ class TaggedCategory(context.TaggedCategory):
 
     def get_undirected_sorting_options(self) -> typing.List[str]:
         return settings.PRODUCTS_ORDERING
+
+
+class ProductImages(context.AbstractProductsListContext):
+
+    @property
+    def images(self) -> typing.Dict[int, Image]:
+        assert isinstance(self.products, ProductQuerySet)
+
+        images = {}
+        if self.product_pages:
+            images = Image.objects.get_main_images_by_pages(
+                self.product_pages.filter(stroyprombeton_product__in=self.products)
+            )
+
+        return {
+            product.id: images.get(product.page)
+            for product in self.products
+        }
+
+    def get_context_data(self):
+        return {
+            'product_images': self.images,
+            **(
+                self.super.get_context_data()
+                if self.super else {}
+            ),
+        }
